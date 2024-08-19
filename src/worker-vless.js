ee63cb782c1c2c589acabcb94465a991d456069c
yeefeel <flyfeel@gmail.com>
2024-08-19 22:59:24 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'e8bb5f85-cfaf-4225-8b4a-ffb324783beb';
 
-let proxyIP = '';
+let proxyIP = 'cdn.xn--b6gac.eu.org';
 
 
 if (!isValidUUID(userID)) {
